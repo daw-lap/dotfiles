b8cafb9f3daac6e616adc77af66f5e6e1e581af3
dl1987 <dawid.lapczynski@gmail.com>
2022-04-16 17:08:25 +0200

    [fish] basic config added

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# oh-my-posh init fish --config $HOME/.poshthemes/robbyrussel.omp.json | source
+# oh-my-posh init fish --config $HOME/.poshthemes/pararussel.omp.json | source
+# oh-my-posh init fish --config $HOME/.poshthemes/amro.omp.json | source
+oh-my-posh init fish --config $HOME/.poshthemes/patriksvensson.omp.json | source
