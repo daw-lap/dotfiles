bf2ba8e390607d418957910b9cb7cacde698458c
daw-lap <dawid.lapczynski@gmail.com>
2025-11-08 21:21:21 +0100

    wezterm: disable wayland

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.enable_wayland = false
+
 config.color_scheme = 'Apprentice (Gogh)'
 
 config.window_background_opacity = 0.95
